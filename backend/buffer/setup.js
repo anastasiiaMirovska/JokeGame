7288edefb015e536ba6c78ea4d1ecebacaa55c3f
Anastasiia Mirovska <anastasiia.mirovska@gmail.com>
2025-04-08 23:50:04 +0300

    Try 34

---
+++ backend/buffer/setup.js
@@ -0,0 +1,28 @@
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+//
+// const setupTestDB = () => {
+//     beforeAll(async () => {
+//         const mongoUri = 'mongodb://user:user@localhost:27018/my_database?authSource=admin';
+//         await mongoose.connect(mongoUri, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//             serverSelectionTimeoutMS: 5000,
+//             retryWrites: false,
+//         });
+//     });
+//
+//     afterAll(async () => {
+//         await mongoose.connection.close();
+//     });
+//
+//     beforeEach(async () => {
+//         // Clear all collections before each test
+//         const collections = mongoose.connection.collections;
+//         for (const key in collections) {
+//             await collections[key].deleteMany();
+//         }
+//     });
+// };
+//
+// module.exports = setupTestDB;
