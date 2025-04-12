bf8b3b105bcaf1b8a7755ac87ff215d0e7cf1d61
Anastasiia Mirovska <anastasiia.mirovska@gmail.com>
2025-04-12 19:40:26 +0300

    fix: rename MONGODB_URI into MONGO_URI

--- backend/__tests__/jokes.test.js
+++ backend/__tests__/jokes.test.js
@@ -10,7 +10,7 @@ describe('Jokes API', () => {
     beforeAll(async () => {
         // Підключаємося до бази даних, що працює в Docker на порту 27018
         jest.setTimeout(30000);
-        await mongoose.connect(process.env.MONGODB_URI, {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
