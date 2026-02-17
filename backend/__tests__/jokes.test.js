902d8b71fd1b7cf042defdf56dab8eed9c26711a
Anastasiia Mirovska <anastasiia.mirovska@gmail.com>
2026-02-18 01:22:49 +0200

    add empty volume

--- backend/__tests__/jokes.test.js
+++ backend/__tests__/jokes.test.js
@@ -11,7 +11,7 @@ describe('Jokes API', () => {
     beforeAll(async () => {
         // Підключаємося до бази даних, що працює в Docker на порту 27018
         jest.setTimeout(30000);
-        await mongoose.connect(config.database.mongoUrl, {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
